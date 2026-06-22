e8678bbcc8ec61485502e53ca16581f025f167b7
Developer <user@example.com>
2026-06-22 23:17:10 +0530

    all Components added from the shadCn ui and setup complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+// import prisma from "@/lib/db";
 import { Button } from "@/components/ui/button"
 
 const Page = () => {
+  // const posts = await prisma.post.findMany();
   return (
     <div>
-      <Button variant="secondary">
-        Hello
-      </Button>
+      {
+        <Button variant="secondary" className="font-black text-5xl border-black">
+          Hello
+        </Button>
+      }
+
+      {/* {JSON.stringify(posts, null, 2)} */}
     </div>
   );
-}
+};
 
 export default Page;
