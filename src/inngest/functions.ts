806203b4dd85bbec202fd44905a53a3294cb8b47
Developer <user@example.com>
2026-06-25 23:47:49 +0530

    Background Jobs setup done using inngest

---
+++ src/inngest/functions.ts
@@ -0,0 +1,19 @@
+// src/inngest/functions.ts
+import { inngest } from "./client";
+
+export const processTask = inngest.createFunction(
+    { id: "process-task", triggers: { event: "app/task.created" } },
+    async ({ event, step }) => {
+        const result = await step.run("handle-task", async () => {
+            return { processed: true, id: event.data.id };
+        });
+
+        await step.sleep("pause", "10s");
+
+        await step.sleep("wait-please", "10s");
+
+        await step.sleep("jsut winding", "10s");
+
+        return { message: `Task ${event.data.id} complete`, result };
+    }
+);
