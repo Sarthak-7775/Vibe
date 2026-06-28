ab70b2590d22530711ca530357bdf3b6441b2f9b
Developer <user@example.com>
2026-06-28 23:48:05 +0530

    e2b sandbox created , docker installed and nextjs added into the e2bSandbox without shadcn

---
+++ sandbox-templates/nextjs/build.prod.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+// to import .env file
+
+import { Template, defaultBuildLogger } from 'e2b'
+import { template } from './template'
+
+async function main() {
+  await Template.build(template, 'vibe-nextjs-sarthak-prod', {
+    onBuildLogs: defaultBuildLogger(),
+  });
+}
+
+main().catch(console.error);
